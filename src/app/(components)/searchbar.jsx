16de3b72ec267e2f1d48ab6030f8881dec3d7e1b
abimbola7 <abimbolababajide6@gmail.com>
2024-03-21 12:25:18 +0100

    fix

--- src/app/(components)/searchbar.jsx
+++ src/app/(components)/searchbar.jsx
@@ -6,7 +6,7 @@ import { CiSearch } from "react-icons/ci";
 const Searchbar = ({ className }) => {
   const [ search, setSearch ] = React.useState('')
   return (
-    <form className={`relative p-2 border rounded-full shadow text-gray-600 bg-white dark:bg-accent ${className}`}>
+    <form className={`relative p-2 border rounded-full shadow text-gray-600 bg-white dark:bg-accent ${className} dark:border-gray-700`}>
       <div className="absolute">
         <CiSearch 
         size={20}
